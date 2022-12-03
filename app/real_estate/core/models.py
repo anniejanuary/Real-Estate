cbe75de34798ab1396ca5fece04dfaa94f52ef8f
tom <t-gutowski@o2.pl>
2022-12-03 17:41:38 +0100

    feat(user): user model
    Add User model to be mapped in database, implemented model migration and add additonal fields in settings.

--- app/real_estate/core/models.py
+++ app/real_estate/core/models.py
@@ -1,3 +1,28 @@
+"""Database models."""
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for system Users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Class for system User."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
