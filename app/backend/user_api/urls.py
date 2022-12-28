3410b28451e0ef7efc56881923efac94083c1529
tom <t-gutowski@o2.pl>
2022-12-28 11:34:21 +0100

    feat(api): user info endpoint
    Add logged in user info retrieval on /me endpoint.

--- app/backend/user_api/urls.py
+++ app/backend/user_api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from user_api import views
 
 app_name = "user_api"
+
 urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create"),
+    path("me/", views.ManageUserView.as_view(), name="me"),
 ]
