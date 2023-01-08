8289d8c7fe1644b4a01de8d8267c9acd3dfa451e
tom <t-gutowski@o2.pl>
2023-01-08 16:29:44 +0100

    feat: add login page
    With simple log in form - not connected with beckend yet.

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="col-6 mx-auto">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">LogIn</router-link>
+    </nav>
+  </div>
   <router-view/>
 </template>
 
